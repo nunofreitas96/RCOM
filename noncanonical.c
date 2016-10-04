890d9e5d675a6f1b78fc007ac16352b9a5fd0466
Antonio-Melo <happyd3nt@gmail.com>
2016-10-04 11:18:10 +0100

    1st commit

---
+++ noncanonical.c
@@ -0,0 +1,108 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <string.h>
+
+#define BAUDRATE B9600
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+volatile int STOP=FALSE;
+
+int main(int argc, char** argv)
+{
+    int fd,c, res, len;
+    struct termios oldtio,newtio;
+    char buf[255];
+    char str[255];
+
+    if ( (argc < 2) || 
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+    }
+
+
+  /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+  
+    
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(argv[1]); exit(-1); }
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 1 chars received */
+
+
+
+  /* 
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+    leitura do(s) próximo(s) caracter(es)
+  */
+
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+    printf("New termios structure set\n");
+	
+	
+	int i = 0;
+    while (STOP==FALSE) {
+            
+      res = read(fd,buf+i,1); 
+      i++;
+      if(buf[i-1] == '\0') 
+         STOP = TRUE;    
+      
+	
+    }
+	printf("Message received: %s\n", buf);
+
+	int tam = strlen(buf);
+    	res = write(fd,buf,tam+1);   
+	sleep(1);
+    	printf("%d bytes written\n", res);
+
+      
+/*
+      len = strlen(str);
+      res = write(fd, str, len+1);
+      printf("%d bytes written\n", len+1);*/
+
+  /* 
+    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião 
+  */
+
+    //sleep(5);
+
+    tcsetattr(fd,TCSANOW,&oldtio);
+    close(fd);
+    return 0;
+}
