85d0554c63df6acff337bccafe51defabe4ea030
Francisco Pinho <Holykael1@users.noreply.github.com>
2016-11-01 18:37:10 +0000

    transmitter and utilities

--- utils.h
+++ utils.h
@@ -1,7 +1,19 @@
 #ifndef __UTILS_H
 #define __UTILS_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <math.h>
 
+#define MODEMDEVICE "/dev/ttyS1"
 #define BAUDRATE B9600
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
@@ -9,6 +21,8 @@
 #define ERR 0x36
 #define ERR2 0x35
 #define PACKET_SIZE 2048
+#define FLAG 0x7E
+
 int status = TRUE;
 volatile int STOP=FALSE;
 volatile int readFile=FALSE;
